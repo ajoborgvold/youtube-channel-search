c8722b07fa03247bc998f6fa04582f40f2e2d29f
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-06-28 16:56:23 +0200

    updated serverless function

--- netlify/functions/callApi/callApi.js
+++ netlify/functions/callApi/callApi.js
@@ -1,13 +1,18 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const apiKey = process.env.API_KEY
+const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+
 const handler = async (event) => {
+  const channelId = event.body.channelId
+  const searchValue = event.body.searchValue
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const response = await fetch(`${baseUrl}&key=${apiKey}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({
+        reply: response.data
+      }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }

0b7e60a9073fd08f96d751040f9795659a033cf6
ajoborgvold <115482909+ajoborgvold@users.noreply.github.com>
2023-09-19 07:57:38 +0200

    Add updated files via upload

--- contextProvider/Context.jsx
+++ contextProvider/Context.jsx
@@ -19,68 +19,120 @@ function ContextProvider({children}) {
     function handleSearchFieldChange(e) {
         setSearchValue(e.target.value)
     }
-        
-    async function searchForVideos(e) {
-        console.log('serverless function called')
-
+    
+    function searchForVideos(e) {
         e.preventDefault()
-
-        const url = 'https://frolicking-alpaca-235d75.netlify.app/.netlify/functions/callApi'
-
-        const response = await fetch(url, {
-            method: 'POST',
-            header: {
-                'content-type': 'text/plain'
-            },
-            body: {
-                channelId: channelId,
-                searchValue: searchValue
-            }
-        })
-        const data = await response.json()
-        console.log(data)
-
-
-        // const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
-        // const apiKey = process.env.API_KEY
-
-        // fetch(`${baseUrl}&key=${apiKey}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
-        //     .then(res => {
-        //         if (!res.ok) {
-        //             setHasData(false)
-        //             console.log('res.ok === false')
-        //             setErrorMessage('The search engine is currently unavailable. Please try again later.')
-        //         }
-        //         else {
-        //             return res.json()
-        //         }
-        //     })
-        //     .then(data => {
-        //         if (!data) {
-        //             setHasData(false)
-        //             console.log('No data found')
-        //             setErrorMessage('The search engine is currently unavailable. Please try again later.')
-        //             return
-        //         }
-        //         else if (!data.items.length) {
-        //             setHasData(false)
-        //             console.log('No data found')
-        //             setErrorMessage('Sorry, there are no videos matching your search. Please try another search.')
-        //         }
-        //         else {
-        //             setHasData(true)
-        //             setVideosData(data.items)
-        //         }
-        //     })
-    }
+        const URL = "https://tangerine-torte-2fd1e5.netlify.app/.netlify/functions/fetch-videos";
         
-    return (
-        <Context.Provider 
-            value={{searchValue, handleSearchFieldChange, handleSelectChange, videosData, searchForVideos, buttonClass, errorMessage, hasData }}
-        >
+        fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ channelId, searchValue }),
+        })
+        .then((res) => {
+            if (!res.ok) {
+                setHasData(false);
+                console.log("res.ok === false");
+                setErrorMessage(
+                    "The search engine is currently unavailable. Please try again later."
+                    );
+                } else {
+                    return res.json();
+                }
+            })
+            .then((data) => {
+                if (!data) {
+                    setHasData(false);
+                    console.log("No data found");
+                    setErrorMessage(
+                        "The search engine is currently unavailable. Please try again later."
+                        );
+                        return;
+                    } else if (!data.items.length) {
+                        setHasData(false);
+                        console.log("No data found");
+                        setErrorMessage(
+                            "Sorry, there are no videos matching your search. Please try another search."
+                            );
+                        } else {
+                            setHasData(true);
+                            setVideosData(data.items);
+                        }
+                    });
+                }
+                
+                return (
+                    <Context.Provider 
+                    value={{
+                        searchValue,
+                        handleSearchFieldChange,
+                        handleSelectChange,
+                        videosData,
+                        searchForVideos,
+                        buttonClass,
+                        errorMessage,
+                        hasData
+                    }}
+                    >
             {children}
         </Context.Provider>
     )
 }
 
 export {ContextProvider, Context}
+
+// async function searchForVideos(e) {
+//     console.log('serverless function called')
+
+//     e.preventDefault()
+
+//     const url = 'https://frolicking-alpaca-235d75.netlify.app/.netlify/functions/callApi'
+
+//     const response = await fetch(url, {
+//         method: 'POST',
+//         header: {
+//             'content-type': 'text/plain'
+//         },
+//         body: {
+//             channelId: channelId,
+//             searchValue: searchValue
+//         }
+//     })
+//     const data = await response.json()
+//     console.log(data)
+
+
+// const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+// const apiKey = process.env.API_KEY
+
+// fetch(`${baseUrl}&key=${apiKey}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
+//     .then(res => {
+//         if (!res.ok) {
+//             setHasData(false)
+//             console.log('res.ok === false')
+//             setErrorMessage('The search engine is currently unavailable. Please try again later.')
+//         }
+//         else {
+//             return res.json()
+//         }
+//     })
+//     .then(data => {
+//         if (!data) {
+//             setHasData(false)
+//             console.log('No data found')
+//             setErrorMessage('The search engine is currently unavailable. Please try again later.')
+//             return
+//         }
+//         else if (!data.items.length) {
+//             setHasData(false)
+//             console.log('No data found')
+//             setErrorMessage('Sorry, there are no videos matching your search. Please try another search.')
+//         }
+//         else {
+//             setHasData(true)
+//             setVideosData(data.items)
+//         }
+//     })
+// }
--- netlify/functions/callApi/callApi.js
+++ netlify/functions/callApi/callApi.js
@@ -1,24 +1,79 @@
-const apiKey = process.env.API_KEY
-const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+// const apiKey = process.env.API_KEY
+// const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
 
-const handler = async (event) => {
-  console.log('api called from serverless function')
+// const handler = async (event) => {
+//   console.log('api called from serverless function')
 
-  const channelId = event.body.channelId
-  const searchValue = event.body.searchValue
+//   const channelId = event.body.channelId
+//   const searchValue = event.body.searchValue
 
+//   try {
+//     const response = await fetch(`${baseUrl}&key=${apiKey}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         reply: response.data
+//       }),
+//     }
+//   } catch (error) {
+//     return { statusCode: 500, body: error.toString() }
+//   }
+// }
+
+// module.exports = { handler }
+
+exports.handler = async (event) => {
   try {
-    const response = await fetch(`${baseUrl}&key=${apiKey}&maxResults=10&channelId=${channelId}&q=${searchValue}`)
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        reply: response.data
-      }),
+    // Handle CORS preflight request.
+    if (event.httpMethod === "OPTIONS") {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Origin": "*", // Allow from anywhere
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        },
+        body: "",
+      };
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
 
-module.exports = { handler }
+    // Handle POST request.
+    if (event.httpMethod === "POST") {
+      const { channelId, searchValue } = JSON.parse(event.body);
+
+      // const YOUTUBE_API_KEY = "AIzaSyC-0bMXoBYRfR1Gp5k1JCVNy38cgIX_IHk"
+      const YOUTUBE_API_KEY = process.env.API_KEY
+
+
+      const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${YOUTUBE_API_KEY}&maxResults=10&channelId=${channelId}&q=${searchValue}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Origin": "*", // Allow from anywhere
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        },
+        body: JSON.stringify(data),
+      };
+    }
+
+    // If the request HTTP method is neither OPTIONS nor POST, return a method not allowed message.
+    return {
+      statusCode: 405,
+      headers: {
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      },
+      body: JSON.stringify({ message: "Method not allowed" }),
+    };
+
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};

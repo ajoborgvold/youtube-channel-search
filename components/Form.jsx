5d8fa8797ad9ea48289f7d213b7f05291faf7613
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-19 08:05:55 +0200

    Format Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,38 +4,39 @@ import channelData from '../data/channelData'
 
 function Form() {
     const {
-        searchValue, 
-        handleSearchFieldChange, 
-        handleSelectChange, 
-        buttonClass, 
+        searchValue,
+        handleSearchFieldChange,
+        handleSelectChange,
+        buttonClass,
         searchForVideos
-        } = useContext(Context)
-    
+    } = useContext(Context)
+
     const optionElements = channelData.map(item => {
         return <option key={item.id} value={item.id}>{item.title}</option>
     })
-    
+
     return (
         <section className="form-container">
             <form>
                 <label htmlFor="select-channel">Choose a YouTube channel</label>
-                <select 
-                    name="select-channel" 
-                    id="select-channel" 
+                <select
+                    name="select-channel"
+                    id="select-channel"
                     onChange={e => handleSelectChange(e)}
                 >
-                    <>{optionElements}</>
+                    {optionElements}
                 </select>
                 <label htmlFor="search-field">Search for videos</label>
-                <input 
-                    type="text" 
-                    name="search-field" 
+                <input
+                    type="text"
+                    name="search-field"
                     id="search-field"
-                    value={searchValue} 
+                    value={searchValue}
                     placeholder="Search"
                     required
-                    onChange={e => handleSearchFieldChange(e)}/>
-                <button 
+                    onChange={e => handleSearchFieldChange(e)}
+                />
+                <button
                     className={buttonClass}
                     onClick={e => searchForVideos(e)}>
                     <i className="fa-solid fa-arrow-right"></i>

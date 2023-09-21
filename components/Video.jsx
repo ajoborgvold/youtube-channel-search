20a0b7b10854539c13a6a60aa5a2e727841e094d
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-21 10:31:28 +0200

    Fixed bug in Video.jsx
    Fixed bug that caused the onMouseEnter event listener to set all items as hovered. Now only the targeted item is set as hovered

---
+++ components/Video.jsx
@@ -0,0 +1,55 @@
+import { useState, useContext } from 'react'
+import { Context } from '../contextProvider/Context'
+
+function Video() {
+    const { videosData } = useContext(Context)
+    const [hoveredItem, setHoveredItem] = useState(null)
+    console.log(hoveredItem)
+    
+    //--- Filter out channels from the videosData, keep only videos and playlists ---//
+    const videosToDisplay = videosData.filter(item => !item.id.channelId)
+
+    const videoElements = videosToDisplay.map(item => {
+        //--- Check if item is a video or a playlist and define id, url and description accordingly ---//
+        const itemId = item.id.videoId ? (item.id.videoId) : (item.id.playlistId)
+
+        const url = item.id.videoId ?
+            `https://www.youtube.com/watch?v=${item.id.videoId}` :
+            `https://www.youtube.com/playlist?list=${item.id.playlistId}`
+        
+        const description = item.id.videoId ?
+            (item.snippet.description) :
+            `A playlist by ${item.snippet.channelTitle}`
+
+        return (
+            <div
+                key={itemId}
+                className="video-item"
+                onMouseEnter={() => setHoveredItem(itemId)}
+                onMouseLeave={() => setHoveredItem(null)}
+            >
+                <div className="image-wrapper">
+                    <a
+                        href={url}
+                        target="_blank">
+                        <img
+                            src={item.snippet.thumbnails.medium.url}
+                            className="thumbnail"
+                            alt={description}
+                        />
+                        {hoveredItem === itemId && <i className="fa-solid fa-play play-movie"></i>}
+                    </a>
+                </div>
+                <h3 
+                    className="video__title" 
+                    dangerouslySetInnerHTML={{ __html: `${item.snippet.title} (${item.snippet.publishedAt.substring(0, 4)})` }}
+                />
+            </div>
+        )
+
+    })
+
+    return <>{videoElements}</>
+}
+
+export default Video

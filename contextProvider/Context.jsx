2c07dfae9a0d21934f8df27355683a8077d54dc0
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-21 09:15:12 +0200

    Update Context.jsx
    Created new variables in Context to handle loading state and to set a new videos heading displaying the selected channel and the search

--- contextProvider/Context.jsx
+++ contextProvider/Context.jsx
@@ -3,31 +3,46 @@ import channelData from '../data/channelData'
 
 const Context = React.createContext()
 
-function ContextProvider({children}) {
+function ContextProvider({ children }) {
     const [channelId, setChannelId] = useState(channelData[0].id)
     const [searchValue, setSearchValue] = useState('frontend')
     const [videosData, setVideosData] = useState([])
     const [errorMessage, setErrorMessage] = useState('')
     const [hasData, setHasData] = useState(false)
-    
+    const [videosHeading, setVideosHeading] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(() => {
         searchForVideos()
     }, [])
 
     const buttonClass = searchValue ? 'enabled' : 'disabled'
-        
+
     function handleSelectChange(e) {
         setChannelId(e.target.value)
     }
-        
+
     function handleSearchFieldChange(e) {
         setSearchValue(e.target.value)
     }
-    
+
+    function getVideosHeading() {
+        const selectedChannel = channelData.filter(item => item.id === channelId && item)[0].title
+        const selectedSearch = searchValue
+        
+        setVideosHeading({
+            channel: selectedChannel,
+            search: selectedSearch
+        })
+    }
+
     function searchForVideos(e) {
         e && e.preventDefault()
+        setIsLoading(true)
+        getVideosHeading()
+
         const URL = "https://tangerine-torte-2fd1e5.netlify.app/.netlify/functions/fetch-videos";
-        
+
         fetch(URL, {
             method: "POST",
             headers: {
@@ -35,12 +50,12 @@ function ContextProvider({children}) {
             },
             body: JSON.stringify({ channelId, searchValue }),
         })
-        .then((res) => {
-            if (!res.ok) {
-                setHasData(false);
-                console.log("res.ok === false");
-                setErrorMessage(
-                    "The search engine is currently unavailable. Please try again later."
+            .then((res) => {
+                if (!res.ok) {
+                    setHasData(false);
+                    console.log("res.ok === false");
+                    setErrorMessage(
+                        "The search engine is currently unavailable. Please try again later."
                     );
                 } else {
                     return res.json();
@@ -52,37 +67,41 @@ function ContextProvider({children}) {
                     console.log("No data found");
                     setErrorMessage(
                         "The search engine is currently unavailable. Please try again later."
-                        );
-                        return;
-                    } else if (!data.items.length) {
-                        setHasData(false);
-                        console.log("No data found");
-                        setErrorMessage(
-                            "Sorry, there are no videos matching your search. Please try another search."
-                            );
-                        } else {
-                            setHasData(true);
-                            setVideosData(data.items);
-                        }
-                    });
+                    );
+                    return;
+                } else if (!data.items.length) {
+                    setHasData(false);
+                    console.log("No data found");
+                    setErrorMessage(
+                        "Sorry, there are no videos matching your search. Please try another search."
+                    );
+                } else {
+                    setHasData(true);
+                    setVideosData(data.items);
+                    setIsLoading(false)
                 }
-                
-                return (
-                    <Context.Provider 
-                    value={{
-                        searchValue,
-                        handleSearchFieldChange,
-                        handleSelectChange,
-                        videosData,
-                        searchForVideos,
-                        buttonClass,
-                        errorMessage,
-                        hasData
-                    }}
-                    >
+            });
+    }
+
+    return (
+        <Context.Provider
+            value={{
+                searchValue,
+                handleSearchFieldChange,
+                handleSelectChange,
+                videosData,
+                searchForVideos,
+                buttonClass,
+                errorMessage,
+                hasData,
+                channelId,
+                videosHeading,
+                isLoading
+            }}
+        >
             {children}
         </Context.Provider>
     )
 }
 
-export {ContextProvider, Context}
+export { ContextProvider, Context }

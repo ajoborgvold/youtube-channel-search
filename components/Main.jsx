0cf7246ea2aeb9953e5c98ff730303b5843f3135
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-21 17:05:10 +0200

    Change loader icon in Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,19 +2,21 @@ import { useContext } from 'react'
 import Video from './Video'
 import Error from './Error'
 import { Context } from '../contextProvider/Context'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { ImSpinner3 } from 'react-icons/im'
 
 function Main() {
     const { hasData, videosHeading, isLoading } = useContext(Context)
 
     return (
         <main>
-            <h2 className="main__subheading">{`${videosHeading.channel}: ${videosHeading.search}`}</h2>
+            <h2 className="main__subheading">{hasData ? `${videosHeading.channel}: ${videosHeading.search}` : null}</h2>
             <div className="video-container">
-                {isLoading ?
-                    <FontAwesomeIcon icon={faSpinner} className='loading-icon'/> :
+                {/* {isLoading ?
+                    <ImSpinner3 className='loading-icon'/> :
                     hasData ? <Video /> : <Error />
+                } */}
+                {!hasData ? <Error /> :
+                    isLoading ? <ImSpinner3 className='loading-icon'/> : <Video />
                 }
             </div>
         </main>

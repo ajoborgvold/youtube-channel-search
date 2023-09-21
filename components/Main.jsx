e53f31d1976b3e93d37a5af33156c0cdbdfa992f
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-21 10:26:19 +0200

    Created loading spinner in Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,6 +2,8 @@ import { useContext } from 'react'
 import Videos from './Videos'
 import Error from './Error'
 import { Context } from '../contextProvider/Context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 function Main() {
     const { hasData, videosHeading, isLoading } = useContext(Context)
@@ -11,7 +13,9 @@ function Main() {
             <h2 className="main__subheading">{`${videosHeading.channel}: ${videosHeading.search}`}</h2>
             <div className="video-container">
                 {isLoading ?
-                    <p>Loading...</p> :
+                    // <p className='error'>Loading...</p> :
+                    // <FontAwesomeIcon icon="fa-solid fa-spinner" spinPulse style={{color: "#1b4965",}} /> :
+                    <FontAwesomeIcon icon={faSpinner} className='loading-icon'/> :
                     hasData ? <Videos /> : <Error />
                 }
             </div>

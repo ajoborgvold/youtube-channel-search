a451d18a55fed7d0eb6319ebc5352fbb0c3897da
ajo <115482909+ajoborgvold@users.noreply.github.com>
2023-09-22 12:36:22 +0200

    Rephrase header__subheading

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
                 <i className="fa-solid fa-play"></i>
                 <h1>Channel <span className="light-blue">Search</span></h1>
             </div>
-            <h2 className="header__subheading">Seach for videos about programming from popular YouTube channels</h2>
+            <h2 className="header__subheading">Seach for videos about coding from popular YouTube channels</h2>
         </header>
     )
 }
